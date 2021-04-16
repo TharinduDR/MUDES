f2f7413f9c683194253f7ea9286587bad3058396
TharinduDR <rhtdranasinghe@gmail.com>
2021-04-16 12:51:59 +0100

    003: Adding spacey lib

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mudes",
-    version="0.2.3",
+    version="0.3.0",
     author="Tharindu Ranasinghe",
     author_email="rhtdranasinghe@gmail.com",
     description="Toxic Spans Prediction",
